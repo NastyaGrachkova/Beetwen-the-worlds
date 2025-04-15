7275e8b10f1932525ae44b61d3402fe3973be24e
NastyaGrachkova <tvgnn@hotmail.com>
2025-04-15 20:05:38 +0300

    Change dialog

--- new_game/Assets/Scripts/DialogSystem/DialogHandler.cs
+++ new_game/Assets/Scripts/DialogSystem/DialogHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class DialogHandler
@@ -5,6 +6,7 @@ public class DialogHandler
     private DialogData _dialogData;
     private DialogView _dialogView;
     private int _dialogIndex;
+    private WaitForSeconds _waitForSeconds = new WaitForSeconds(0.01f);
 
     public DialogHandler(DialogData dialogData, DialogView dialogView)
     {
@@ -21,7 +23,19 @@ public class DialogHandler
     private void ShowElementFromDialogData(DialogElements dialogElements)
     {
         _dialogView.Portret.sprite = dialogElements.PortretSprite;
-        _dialogView.Text.text = dialogElements.DialogText;
+        //_dialogView.Text.text = dialogElements.DialogText;
+        _dialogView.StartCoroutine(TypeText(dialogElements.DialogText));
         _dialogIndex++;
+    }
+
+    private IEnumerator TypeText(string FullText)
+    {
+        _dialogView.Text.text = "";
+        for (int i = 0; i < FullText.Length; i++)
+        {
+            _dialogView.Text.text += FullText[i];
+            yield return _waitForSeconds;
+        }
+        
     }
 }

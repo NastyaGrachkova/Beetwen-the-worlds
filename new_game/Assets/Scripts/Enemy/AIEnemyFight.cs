5fc261ac990bd2f9426ce32c8036b6df00e24736
NastyaGrachkova <tvgnn@hotmail.com>
2025-05-01 22:18:58 +0300

    Healhy for enemies + CoinsText + PatrolEnemy

--- new_game/Assets/Scripts/Enemy/AIEnemyFight.cs
+++ new_game/Assets/Scripts/Enemy/AIEnemyFight.cs
@@ -1,9 +1,25 @@
+using Pathfinding;
 using System.Collections;
 using UnityEngine;
 
 public class AIEnemyFight : EnemyStats
 {
     [SerializeField] private int _damageAmount = 10; // Количество урона, наносимого игроку
+    [SerializeField] private float _moveSpeed = 2f; // Скорость движения врага
+
+    private SpriteRenderer sprite;
+    [SerializeField] private AIPath aiPath;
+
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        sprite.flipX = aiPath.desiredVelocity.x >= 0.01f;
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
